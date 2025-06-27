022f987976b0bc3732ee7b8384acd7aafc479425
Caglar Cercinli <caglar.cercinli@vrt.be>
2025-06-27 16:32:26 +0200

    update book improvements

--- src/modules/book/service/book.service.ts
+++ src/modules/book/service/book.service.ts
@@ -47,6 +47,44 @@ export class BookService {
     return this.booksRepository.save(book);
   }
 
+  async remove(id: number): Promise<void> {
+    await this.booksRepository.delete(id);
+  }
+  //TODO: transform a (regular book) to a (comic book) and vice versa.
+  async update(id: number, bookDto: BookDto): Promise<void> {
+    const book = await this.booksRepository.findOne({
+      where: { id },
+      relations: ['author'],
+    });
+
+    if (!book) {
+      throw new Error('Book not found');
+    }
+
+    let bookBuilder: BookBuilder;
+    if (bookDto.amountOfPictures) {
+      bookBuilder = new ComicBookBuilder();
+      const book = await this.getComicBook(
+        bookDto,
+        bookBuilder as ComicBookBuilder,
+      );
+      await this.comicBooksRepository.save(book);
+      return;
+    }
+
+    bookBuilder = new BookBuilder().setId(book.id).setAuthor(book.author);
+
+    if (bookDto.name) {
+      bookBuilder.setName(bookDto.name);
+    }
+
+    const author = await this.getAuthor(bookDto.authorId);
+    if (author) bookBuilder.setAuthor(author);
+
+    const updatedBook = bookBuilder.getBook();
+    await this.booksRepository.save(updatedBook);
+  }
+
   private async getComicBook(
     bookDto: BookDto,
     bookBuilder: ComicBookBuilder,
@@ -68,54 +106,5 @@ export class BookService {
       id,
     });
     return author;
-  }
-
-  async remove(id: number): Promise<void> {
-    await this.booksRepository.delete(id);
-  }
-
-  //TODO: transform a (regular book) to a (comic book) and vice versa.
-  async update(id: number, bookDto: BookDto): Promise<void> {
-    const book = await this.booksRepository.findOne({
-      where: { id },
-      relations: ['author'],
-    });
-
-    if (!book) {
-      throw new Error('Book not found');
-    }
-
-    let bookBuilder: BookBuilder;
-    if (bookDto.amountOfPictures) {
-      bookBuilder = new ComicBookBuilder()
-        .setId(book.id)
-        .setName(book.name)
-        .setAuthor(book.author)
-        .setAmountOfPictures(bookDto.amountOfPictures);
-    } else {
-      bookBuilder = new BookBuilder()
-        .setId(book.id)
-        .setName(book.name)
-        .setAuthor(book.author);
-    }
-
-    if (bookDto.name) {
-      bookBuilder.setName(bookDto.name);
-    }
-
-    const authorId = Number(bookDto.authorId);
-    if (authorId) {
-      const author = await this.authorsRepository.findOneBy({
-        id: authorId,
-      });
-      if (author) bookBuilder.setAuthor(author);
-    }
-
-    const updatedBook = bookBuilder.getBook();
-    if (bookDto.amountOfPictures) {
-      await this.comicBooksRepository.save(updatedBook as ComicBook);
-    } else {
-      await this.booksRepository.save(updatedBook);
-    }
   }
 }
